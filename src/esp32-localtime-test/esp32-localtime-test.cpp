517590e7dd0e716cc2cd95bab4266861928cf2e2
Braidan <legohead259@gmail.com>
2022-06-28 22:15:24 -0400

    Added millisecond capability to the timestamp formatter Broke out GPS synchronization to seperate function Cleaned up some of the code

--- src/esp32-localtime-test/esp32-localtime-test.cpp
+++ src/esp32-localtime-test/esp32-localtime-test.cpp
@@ -4,6 +4,7 @@
 #include <ThetisLib.h>
 
 #define GPS_SYNC_INTERVAL 1 // Minutes
+#define GPS_SYNC_TIMEOUT 5000 // ms
 
 const char *monthName[12] = {
   "Jan", "Feb", "Mar", "Apr", "May", "Jun",
@@ -14,6 +15,8 @@ tmElements_t tm;
 
 bool getTime(const char *str);
 bool getDate(const char *str);
+long getmSeconds();
+void syncInternalClockGPS();
 
 void setup() {
     Serial.begin(115200);
@@ -32,13 +35,54 @@ void setup() {
     if (!initGPS()) { // Initialize GPS, halt code if failed
         while(true); // Halt code execution
     }
+    // MicroNMEA::sendSentence(GPS, "$PMTK220,2000"); // Set GPS update rate to 2000 ms
     
+    delay(100);
+    syncInternalClockGPS();
+}
+
+void loop() {
+    static long _lastSyncCheck = millis(); 
+    if (millis() >= _lastSyncCheck + GPS_SYNC_INTERVAL*60000) { // Check if GPS_SYNC_INTERVAL minutes have passed since last sync
+        syncInternalClockGPS();
+        _lastSyncCheck = millis();
+    }
+
+    breakTime(now(), tm);
+    static long _lastSecond = 0;
+    static long _lastMSecond = 0;
+    long curMSecond = millis();
+    if (tm.Second == _lastSecond) {
+        curMSecond = millis() - _lastMSecond;
+        // Serial.println((int) curMSecond); //DEBUG
+    }
+    else {
+        _lastSecond = tm.Second;
+        _lastMSecond = millis();
+        curMSecond = 0;
+    }
+
+    static long _lastPrintCheck = millis();
+    if (millis() >= _lastPrintCheck+900) { // Print the time every second 
+        Serial.printf("%04d-%02d-%02dT%02d:%02d:%02d.%03d\n\r", tm.Year+1970, tm.Month, tm.Day, tm.Hour, tm.Minute, tm.Second, curMSecond);
+        _lastPrintCheck = millis();
+    }
+}
+
+void syncInternalClockGPS() {
+    Serial.println();
+    Serial.print("Attempting to sync internal clock to GPS time...");
+    long timeoutStart = millis();
+    while(!GPS) { // Wait for a GPS message to arrive
+        if (millis() >= timeoutStart+GPS_SYNC_TIMEOUT) return; // Stop attempt, if TIMEOUT occurs
+    }
+
     while(GPS.available()) { // Check for an available GPS message
         char c = GPS.read();
         Serial.print(c); // Debug
         nmea.process(c);
     }
-    if (nmea.isValid()) { // If the GPS has a good fix on startup, reset the internal clock to the GPS time
+    if (nmea.isValid()) { // If the GPS has a good fix, reset the internal clock to the GPS time
         tm.Year = nmea.getYear()-1970;
         tm.Month = nmea.getMonth();
         tm.Day = nmea.getDay();
@@ -47,44 +91,15 @@ void setup() {
         tm.Second = nmea.getSecond();
 
         setTime(makeTime(tm)); // Reset internal clock
+        Serial.println("Done!");
     }
+    else {
+        Serial.println("GPS fix was not valid - did not sync");
+    }
+    Serial.println();
 }
 
-void loop() {
-    static long _lastSyncCheck = millis(); 
-    if (now() >= _lastSyncCheck + GPS_SYNC_INTERVAL*60000) { // Check if GPS_SYNC_INTERVAL minutes have passed since last sync
-        Serial.println();
-        Serial.print("Attempting to sync internal clock to GPS time...");
-        while(!GPS); // Wait for a GPS message to arrive
-
-        while(GPS.available()) { // Check for an available GPS message
-            char c = GPS.read();
-            Serial.print(c); // Debug
-            nmea.process(c);
-        }
-        if (nmea.isValid()) { // If the GPS has a good fix, reset the internal clock to the GPS time
-            tm.Year = nmea.getYear()-1970;
-            tm.Month = nmea.getMonth();
-            tm.Day = nmea.getDay();
-            tm.Hour = nmea.getHour();
-            tm.Minute = nmea.getMinute();
-            tm.Second = nmea.getSecond();
-
-            setTime(makeTime(tm)); // Reset internal clock
-            Serial.println("Done!");
-        }
-        else {
-            Serial.println("GPS fix was not valid - did not sync");
-        }
-        Serial.println();
-    }
-	breakTime(now(), tm);
-	Serial.printf("%04d-%02d-%02dT%02d:%02d:%02d\n\r", tm.Year+1970, tm.Month, tm.Day, tm.Hour, tm.Minute, tm.Second);
-	delay(1000);
-}
-
-bool getTime(const char *str)
-{
+bool getTime(const char *str) {
   int Hour, Min, Sec;
 
   if (sscanf(str, "%d:%d:%d", &Hour, &Min, &Sec) != 3) return false;
@@ -94,8 +109,7 @@ bool getTime(const char *str)
   return true;
 }
 
-bool getDate(const char *str)
-{
+bool getDate(const char *str) {
   char Month[12];
   int Day, Year;
   uint8_t monthIndex;

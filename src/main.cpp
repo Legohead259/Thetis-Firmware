5a55a6c475733604c0a9dfec957d1f52293e27a5
Braidan <legohead259@gmail.com>
2023-05-16 19:17:09 -0400

    Added lines to support logging for the Magneto calibration application

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,15 @@
 
 #define SAMPLE_RATE (100) // Hz
 
+unsigned long logFrequency; // Hz 
+unsigned long logInterval;  // Time between log updates [ms]
+unsigned long logButtonPresses;
+unsigned long logButtonStartTime;
+
 // Flags
 bool isDebugging = false;
+
+void IRAM_ATTR logButtonISR();
 
 void setup() {
     // Casting to int is important as just uint8_t types will invoke the "slave" begin, not the master
@@ -43,14 +50,61 @@ void setup() {
         Serial.println("Failed LIS3MDL");
         while(true);
     }
+
+    if (!digitalRead(SD_CARD_DETECT) || !dataLogger.begin(SD, SD_CS)) { // Initialize SD card filesystem and check if good
+        diagLogger->warn("uSD Card not detected or not working, attempting backup with XTSD card");
+        blinkCode((ErrorCode_t) B1010, AMBER); // Warn that there was an issue with the SD card
+        if (!dataLogger.begin(SD, XTSD_CS)) { // Try to initialize backup writing to the XTSD card
+            while(true) blinkCode(CARD_MOUNT_ERROR_CODE); // Block further code execution
+        }
+    }
+
+    // Attach the log enable button interrupt
+    diagLogger->info("Attaching log enable interrupt...");
+    attachInterrupt(LOG_EN, logButtonISR, FALLING);
+    diagLogger->info("done!");
 }
 
 void loop() {
     pollDSO32();
     pollLIS3MDL();
     
-    Serial.printf("Uni:%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f,%0.3f\n", data.accelX, data.accelY, data.accelZ,
-                                                                                data.gyroX, data.gyroY, data.gyroZ,
-                                                                                data.magX, data.magY, data.magZ);
+    // Update the log with the most recent sample
+    static unsigned long _lastLogTime = micros();
+    if (isLogging && (micros() - _lastLogTime) >= logInterval) { // Check if the log interval has passed
+        unsigned long _logStartTime = micros();
+        char buffer[64];
+        #ifdef MAG_CAL {
+        sprintf(buffer, "%0.3f\t%0.3f\t%0.3f\n", data.magX, data.magY, data.magZ);
+        }
+        #endif
+        #ifdef ACCEL_CAL {
+        sprintf(buffer, "%0.3f\t%0.3f\t%0.3f\n", data.accelX, data.accelY, data.accelZ);
+        }
+        #endif
+        dataLogger.print(buffer);
+        diagLogger->trace("Time to log data: %d us", micros() - _logStartTime);
+        _lastLogTime = micros();
+    }
+
+    // Log Enabling handler
+    static uint8_t _oldButtonPresses = 0;
+    if (logButtonPresses != _oldButtonPresses && !digitalRead(LOG_EN) && millis() >= logButtonStartTime+LOG_BTN_HOLD_TIME) { // Check if BTN0 has been pressed and has been held for sufficient time
+        isLogging = !isLogging;
+        if (isLogging) {
+            dataLogger.start(SD);
+            digitalWrite(SD_CS, LOW);
+        }
+        else {
+            dataLogger.stop();
+        }
+        digitalWrite(LED_BUILTIN, isLogging);
+        _oldButtonPresses = logButtonPresses;
+    }
     delay(50); // Delay 50 ms (20 Hz report rate)
+}
+
+void IRAM_ATTR logButtonISR() {
+    logButtonPresses++;
+    logButtonStartTime = millis();
 }

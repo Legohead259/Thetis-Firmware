154cbfde92de9db87b7fe544bf093d8c66825e66
Braidan <legohead259@gmail.com>
2023-05-15 22:06:06 -0400

    Integrated the latest XIO technologies Fusion library

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,23 @@
 #define __FIRMWARE_VERSION__ "1.0.0"
 #include <ThetisLib.h>
 
-Madgwick filter;
-unsigned long microsPerReading, microsPrevious;
-float accelScale, gyroScale;
+#define SAMPLE_RATE (100) // Hz
 
 // Flags
 bool isDebugging = false;
-bool isIMUAvailable = false;
-bool isLogFileCreated = false;
-bool isIMUCalibrated = true;
+
+// Define calibration (replace with actual calibration data if available)
+const FusionMatrix gyroscopeMisalignment = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+const FusionVector gyroscopeSensitivity = {1.0f, 1.0f, 1.0f};
+const FusionVector gyroscopeOffset = {0.0f, 0.0f, 0.0f};
+const FusionMatrix accelerometerMisalignment = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+const FusionVector accelerometerSensitivity = {1.0f, 1.0f, 1.0f};
+const FusionVector accelerometerOffset = {0.0f, 0.0f, 0.0f};
+const FusionMatrix softIronMatrix = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+const FusionVector hardIronOffset = {0.0f, 0.0f, 0.0f};
+
+FusionOffset offset;
+FusionAhrs ahrs;
 
 void setup() {
     // Casting to int is important as just uint8_t types will invoke the "slave" begin, not the master
@@ -40,45 +48,65 @@ void setup() {
     Serial.println();
 
     if (!initDSO32()) { // Check IMU initialization
+        Serial.println("Failed DSO32");
         while(true); // Block further code execution
     }
 
     if (!initLIS3MDL()) { // Check magnetometer initialization
+        Serial.println("Failed LIS3MDL");
         while(true);
     }
 
-    microsPerReading = 1000000 / 25;
-    microsPrevious = micros();
+    // Initialise algorithms
+    FusionOffsetInitialise(&offset, SAMPLE_RATE);
+    FusionAhrsInitialise(&ahrs);
+
+    // Set AHRS algorithm settings
+    const FusionAhrsSettings settings = {
+            .convention = FusionConventionNwu,
+            .gain = 0.5f,
+            .accelerationRejection = 10.0f,
+            .magneticRejection = 20.0f,
+            .rejectionTimeout = 5 * SAMPLE_RATE, /* 5 seconds */
+    };
+    FusionAhrsSetSettings(&ahrs, &settings);
 }
 
 void loop() {
-    int aix, aiy, aiz;
-    int gix, giy, giz;
-    float ax, ay, az;
-    float gx, gy, gz;
-    float roll, pitch, heading;
+    // Acquire latest sensor data
+    unsigned long timestamp = micros(); // replace this with actual gyroscope timestamp
+    pollDSO32();
+    pollLIS3MDL();
+    FusionVector gyroscope = {data.gyroX, data.gyroY, data.gyroZ};
+    FusionVector accelerometer = {data.accelX, data.accelY, data.accelZ};
+    FusionVector magnetometer = {data.magX, data.magY, data.magZ};
 
-    // check if it's time to read data and update the filter
-    if (micros() - microsPrevious >= microsPerReading) {
+    // Apply calibration
+    gyroscope = FusionCalibrationInertial(gyroscope, gyroscopeMisalignment, gyroscopeSensitivity, gyroscopeOffset);
+    accelerometer = FusionCalibrationInertial(accelerometer, accelerometerMisalignment, accelerometerSensitivity, accelerometerOffset);
+    magnetometer = FusionCalibrationMagnetic(magnetometer, softIronMatrix, hardIronOffset);
 
-        // read raw data from MARG
-        pollDSO32();
-        pollLIS3MDL();
+    // Update gyroscope offset correction algorithm
+    gyroscope = FusionOffsetUpdate(&offset, gyroscope);
 
-        // update the filter, which computes orientation
-        filter.update(data.gyroX, data.gyroY, data.gyroZ, 
-                        data.accelX, data.accelY, data.accelZ, 
-                        data.magX, data.magY, data.magZ);
+    // Calculate delta time (in seconds) to account for gyroscope sample clock error
+    static unsigned long previousTimestamp;
+    const float deltaTime = (float) (timestamp - previousTimestamp) / (float) CLOCKS_PER_SEC;
+    previousTimestamp = timestamp;
 
-        // print the heading, pitch and roll
-        Serial.printf("Orientation: %f, %f, %f\n", 360-filter.getYaw(), filter.getPitch(), filter.getRoll());
-        // Serial.printf("Quaternion: %f, %f, %f, %f\n", filter.w, filter.x, filter.y, filter.z);
+    // Update gyroscope AHRS algorithm
+    FusionAhrsUpdate(&ahrs, gyroscope, accelerometer, magnetometer, deltaTime);
 
-        // increment previous time, so we keep proper pace
-        microsPrevious = microsPrevious + microsPerReading;
-    }
+    // Print algorithm outputs
+    const FusionEuler euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
+    const FusionVector earth = FusionAhrsGetEarthAcceleration(&ahrs);
+
+    // printf("Roll %0.1f, Pitch %0.1f, Yaw %0.1f, X %0.1f, Y %0.1f, Z %0.1f\n",
+    //         euler.angle.roll, euler.angle.pitch, euler.angle.yaw,
+    //         earth.axis.x, earth.axis.y, earth.axis.z);
+    Serial.printf("Orientation: %0.3f, %0.3f, %0.3f\n", euler.angle.yaw, euler.angle.pitch, euler.angle.roll);
 
     // Serial.printf("Orientation: %f, %f, %f\n", 360-getYaw(), getPitch(), getRoll());
     // Serial.printf("Quaternion: %f, %f, %f, %f\n", data.quatW, data.quatX, data.quatY, data.quatZ);
-    // delay(50); // Delay 50 ms (20 Hz report rate)
+    delay(50); // Delay 50 ms (20 Hz report rate)
 }
